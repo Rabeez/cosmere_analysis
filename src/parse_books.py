626ef6fb012b80905002057977e87d6fc8b0f024
Rabeez Riaz <ruoter123@gmail.com>
2025-05-18 15:36:22 +0400

    Cleanup

--- src/parse_books.py
+++ src/parse_books.py
@@ -63,64 +63,6 @@ SERIES2MODE: dict[Series, Literal["break_asterisks", "chapter_x"]] = {
 #             yield token.text
 
 
-# def stream_tokens_by_chapter(
-#     nlp: spacy.language.Language,
-#     file_path: Path,
-#     marker_type: Literal["chapter_x", "break_asterisks"],
-# ) -> Iterator[tuple[int, str]]:
-#     chunk_size = 10_000
-#
-#     if marker_type == "chapter_x":
-#         chapter_pattern = r"^[Cc][Hh][Aa][Pp][Tt][Ee][Rr]\s+(\d+|[A-Za-z]+)$"
-#     else:
-#         chapter_pattern = r"^\* \* \*$"
-#     chapter_regex = re.compile(chapter_pattern, re.MULTILINE | re.IGNORECASE)
-#
-#     chapter_count = 0
-#     current_chapter = chapter_count
-#     current_chunk = ""
-#
-#     with file_path.open("r") as file:
-#         while True:
-#             chunk = file.read(chunk_size)
-#             if not chunk:
-#                 # Process any remaining data in buffer at EoF
-#                 if current_chunk:
-#                     lines = current_chunk.splitlines(keepends=True)
-#                     for line in lines:
-#                         m = chapter_regex.match(line.strip())
-#                         if m:
-#                             chapter_count += 1
-#                             current_chapter = chapter_count
-#                             continue  # Skip marker line
-#                         if line.strip():
-#                             doc = nlp(line)
-#                             for token in doc:
-#                                 if token.is_alpha:
-#                                     yield (current_chapter, token.text)
-#                 break
-#
-#             # Append chunk to current buffer
-#             current_chunk += chunk
-#             lines = current_chunk.splitlines(keepends=True)
-#
-#             # Process all complete lines
-#             for _, line in enumerate(lines[:-1]):  # Exclude last (potentially incomplete) line
-#                 m = chapter_regex.match(line.strip())
-#                 if m:
-#                     chapter_count += 1
-#                     current_chapter = chapter_count
-#                     continue  # Skip marker line
-#                 if line.strip():
-#                     doc = nlp(line)
-#                     for token in doc:
-#                         if token.is_alpha:
-#                             yield (current_chapter, token.text)
-#
-#             # Keep the last line (may be incomplete) for the next chunk
-#             current_chunk = lines[-1] if lines else ""
-
-
 def stream_lines_w_metadata(
     nlp: spacy.language.Language,
     file_path: Path,
